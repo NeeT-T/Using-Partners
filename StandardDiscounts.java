47bb3b42d6c3e0af0a63786b93a51fdb4edfb241
Renato S. Oliveira <50246351+NeeT-T@users.noreply.github.com>
2020-09-12 17:03:05 -0300

    Delete StandardDiscounts.java

--- StandardDiscounts.java
+++
@@ -1,8 +0,0 @@
-package atividadeparadigma;
-public class StandardDiscounts implements IDiscountCalculatorAdapter{
-    
-    @Override
-    public double DiscountCalculator(Sale sale) {
-        return sale.getTotal();
-    }
-}
