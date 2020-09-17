ae9b0f67240f707e9b94cd4e8b37ab6c0a424b93
Renato S. Oliveira <50246351+NeeT-T@users.noreply.github.com>
2020-09-17 10:37:54 -0300

    Update AtividadeParadigma.java

--- atividadeparadigma/AtividadeParadigma.java
+++ atividadeparadigma/AtividadeParadigma.java
@@ -6,10 +6,6 @@ public class AtividadeParadigma {
     public static void main(String[] args) throws ClassNotFoundException {
         TryMakeFactory factory = TryMakeFactory.getInstance();
         try{
-            //OBS: Não coloquei os nomes das classes quando setei as funções no factory
-            //Coloquei nomes mais usuais de alto nivel
-            //Pode-se encontrar esses nomes na função privada sem retorno setSystem() dentro da classe TryMakeFactory
-            //Ou executando o programa.
             IDiscountCalculatorAdapter adapter = factory.getDiscount("INSIRA AQUI O TIPO DE DESCONTO");
             Sale sale = new Sale(adapter);
             System.out.println(sale.DiscountCalculator());
